6bd4c331c2ce4686559df495b7ea3d8fa366851d
michang <minseok128128@gmail.com>
2023-08-09 20:50:01 +0900

    fix bresenham

--- bresenham_line.c
+++ bresenham_line.c
@@ -23,121 +23,39 @@ static void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	}
 }
 
-static void	bresenham_line3(t_data *data, int arr[], t_3d_p p1)
-{
-	int	cnt;
-	int	i;
-
-	i = 0;
-	cnt = 0;
-	while (i < arr[3])
-	{
-		p1.y += arr[1];
-		cnt += arr[2];
-		if (cnt >= arr[3])
-		{
-			p1.x += arr[0];
-			cnt -= arr[3];
-		}
-		my_mlx_pixel_put(data, p1.x, p1.y, p1.c);
-		i++;
-	}
-}
-
-static void	bresenham_line2(t_data *data, int arr[], t_3d_p p1)
-{
-	int	cnt;
-	int	i;
-
-	i = 0;
-	cnt = 0;
-	while (i < arr[2])
-	{
-		p1.x += arr[0];
-		cnt += arr[3];
-		if (cnt >= arr[2])
-		{
-			p1.y += arr[1];
-			cnt -= arr[2];
-		}
-		my_mlx_pixel_put(data, p1.x, p1.y, p1.c);
-		i++;
-	}
-}
-
-static void	bresenham_line1(t_data *data, int arr[], t_3d_p p1)
-{
-	if (arr[2] >= arr[3])
-		bresenham_line2(data, arr, p1);
-	else
-		bresenham_line3(data, arr, p1);
-}
-
-void	bresenham_line(t_data *data, t_3d_p p1, t_3d_p p2)
-{
-	int	arr[4];
-
-	arr[0] = 0;
-	arr[1] = 0;
-	arr[2] = p2.x - p1.x;
-	arr[3] = p2.y - p1.y;
-	if (arr[2] < 0)
-	{
-		arr[0] = -1;
-		arr[2] = -arr[2];
-	}
-	else
-		arr[0] = 1;
-	if (arr[3] < 0)
-	{
-		arr[1] = -1;
-		arr[3] = -arr[3];
-	}
-	else
-		arr[1] = 1;
-	bresenham_line1(data, arr, p1);
-}
-
-void	set_bresenham()
+void	set_bresenham(int *arr, t_3d_p p1, t_3d_p p2)
 {
 	arr[0] = round(p1.x);
 	arr[1] = round(p1.y);
 	arr[2] = round(p2.x);
 	arr[3] = round(p2.y);
-	arr[4] = abs(x2 - x1);
-	arr[5] = abs(y2 - y1);
-	arr[6] = (x1 < x2) * -2 + 1;;
-	arr[7] = 
-	arr[8] = 
-	int x1 = round(p1.x);
-	int y1 = round(p1.y);
-	int x2 = round(p2.x);
-	int y2 = round(p2.y);
-	int dx = abs(x2 - x1);
-	int dy = abs(y2 - y1);
-	int sx = (x1 < x2) ? 1 : -1;
-	int sy = (y1 < y2) ? 1 : -1;
-	int err = dx - dy;
+	arr[4] = abs(arr[2] - arr[0]);
+	arr[5] = abs(arr[3] - arr[1]);
+	arr[6] = (arr[0] < arr[2]) * 2 - 1;
+	arr[7] = (arr[1] < arr[3]) * 2 - 1;
+	arr[8] = arr[4] - arr[5];
 }
 
 void	bresenham(t_data *data, t_3d_p p1, t_3d_p p2)
 {
 	int	arr[9];
+
+	set_bresenham(arr, p1, p2);
 	while (1)
 	{
-		my_mlx_pixel_put(data, x1, y1, p1.c);
-		if (x1 == x2 && y1 == y2)
+		my_mlx_pixel_put(data, arr[0], arr[1], p1.c);
+		if (arr[0] == arr[2] && arr[1] == arr[3])
 			break;
-		int e2 = 2 * err;
-		if (e2 > -dy)
+		int e2 = 2 * arr[8];
+		if (e2 > -arr[5])
 		{
-			err -= dy;
-			x1 += sx;
+			arr[8] -= arr[5];
+			arr[0] += arr[6];
 		}
-		if (e2 < dx)
+		if (e2 < arr[4])
 		{
-			err += dx;
-			y1 += sy;
+			arr[8] += arr[4];
+			arr[1] += arr[7];
 		}
 	}
 }
